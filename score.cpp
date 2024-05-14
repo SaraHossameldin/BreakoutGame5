3876a9d28a0d1535a2b0d78f56c9aa23e5909ce1
mariana-hanna <159145980+mariana-hanna@users.noreply.github.com>
2024-05-14 20:42:21 +0300

    Update score.cpp

--- score.cpp
+++ score.cpp
@@ -1,3 +1,34 @@
-#include "score.h"
+#include "Score.h"
+#include "Game.h"
+#include "level.h"
+#include <QFont>
+#include <QDebug>
+extern Game* game;
+Score::Score(QGraphicsTextItem *parent): QGraphicsTextItem(parent){
+    score = 0;
+    setPlainText(QString("Score: ") + QString::number(score));
+    setDefaultTextColor(Qt::black);
+    setFont(QFont("times", 16));
+}
 
-Score::Score() {}
+void Score::increase(){
+
+    if(game->gameOver) {
+        return;
+    }
+
+    score++;
+    setPlainText(QString("Score: ") + QString::number(score));
+    if(score == game->lvl->getmaxScore(game->lvl->getLvl())){
+        game->lvl->lvlUp();
+        int level = game->lvl->getLvl();
+        int val = game->lvl->getLvlVal(level);
+
+        game->nextLevel(val);
+    }
+
+}
+
+int Score::getScore(){
+    return score;
+}
